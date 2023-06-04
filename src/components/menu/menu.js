99494d38aa753a4c959d09bd5181906df6dbfe6c
Geoff George <geoffgeorgethommana@gmail.com>
2023-06-04 20:13:38 +0530

    sidebar

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -6,24 +6,24 @@ const Menu = ({menuOpen,setmenuOpen}) => {
     <div className={'menu '+ (menuOpen && "active")}>
 
     <ul>
-      <li>
+      <li onClick={()=>setmenuOpen(false)}>
         <a href='#intro'>Intro </a>
 
       </li>
 
-      <li>
+      <li onClick={()=>setmenuOpen(false)}>
       <a href='#works'> Works</a>
       </li>
 
-      <li>
+      <li onClick={()=>setmenuOpen(false)}>
       <a href='#home'> Home</a>
       </li>
 
-      <li>
+      <li onClick={()=>setmenuOpen(false)}>
       <a href='#projects'>projects </a>
       </li>
 
-      <li>
+      <li onClick={()=>setmenuOpen(false)}>
       <a href='#contact'>Contact </a>
       </li>
     </ul>
