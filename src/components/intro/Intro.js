e3b0176f562de7185d18a1093388893ae190d5a9
Geoff George <geoffgeorgethommana@gmail.com>
2023-06-04 22:59:19 +0530

    intro

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import './intro.scss';
 
 const Intro = () => {
   return (
-    <div className='intro' id='intro'>intro</div>
+    <div className='intro' id='intro'>
+
+    <div className='left'>
+      <div className='imgContainer'>
+        <img src='assets/man.png' alt='man'></img>
+
+      </div>
+
+    </div>
+
+    <div className='right'>
+
+      <div className='wrapper'>
+        <h2>Hi there</h2>
+        <h1>Im Geoff</h1>
+        <h3>Im a Developer</h3>
+
+        <a href='#projects'>
+          <img src='/assets/down.png' alt='down'></img>
+        </a>
+      </div>
+
+    </div>
+
+    </div>
   )
 }
 
