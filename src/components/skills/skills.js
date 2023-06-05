4f3726b44e72db1605f83edf671a9471a7416724
Geoff George <geoffgeorgethommana@gmail.com>
2023-06-06 00:10:53 +0530

    skills

--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -12,14 +12,14 @@ const Skills = () => {
 
         <SkillItem src='https://github.com/geoffgeorgein/rportfolio/blob/main/public/assets/html5.png?raw=true' title='HTML'/>
         <SkillItem src='https://github.com/geoffgeorgein/rportfolio/blob/main/public/assets/css.png?raw=true' title='CSS'/>
-        <SkillItem src='' title='JavaScript'/>
-        <SkillItem src='' title='ReactJs'/>
-        <SkillItem src='' title='Redux'/>
-        <SkillItem src='' title='NextJs'/>
-        <SkillItem src='' title='NextJs'/>
+        <SkillItem src='https://github.com/geoffgeorgein/rportfolio/blob/main/public/assets/JavaScript-logo.png?raw=true' title='JavaScript'/>
+        <SkillItem src='https://github.com/geoffgeorgein/rportfolio/blob/main/public/assets/React-icon.svg.png?raw=true' title='ReactJs'/>
+        <SkillItem src='https://github.com/geoffgeorgein/rportfolio/blob/main/public/assets/redux.png?raw=true' title='Redux'/>
+        <SkillItem src='https://github.com/geoffgeorgein/rportfolio/blob/main/public/assets/next-js-logo-7929BCD36F-seeklogo.com.png?raw=true' title='NextJs'/>
+        <SkillItem src='https://github.com/geoffgeorgein/rportfolio/blob/main/public/assets/Git-logo.svg.png?raw=true' title='Git'/>
 
-        <SkillItem src='' title='NextJs'/>
-        <SkillItem src='' title='NextJs'/>
+        <SkillItem src='https://github.com/geoffgeorgein/rportfolio/blob/main/public/assets/firebase.png?raw=true' title='Firebase'/>
+        
     </div>
     <p></p>
     <p></p>
