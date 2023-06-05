babdfd75e20c91f7e21f777952a94e68a470d300
Geoff George <geoffgeorgethommana@gmail.com>
2023-06-05 15:34:46 +0530

    projects

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -15,19 +15,32 @@ const Projects = () => {
 
                     <div className='leftContainer'>
 
+                        <div className='imgContainer'>
+                            <img src='' >
+
+                            </img>
+                        </div>
+
+                        <h2>Netflix Clone</h2>
+                        <p> A  Netflix Clone made using React frontend and firebase backend</p>
+                        <span>Projects</span>
+                    </div>
+                </div>
+
+                <div className='right'>
+
+                <div className='rightContainer'>
+
                     <div className='imgContainer'>
-                        <img src=''>
+                        <img src='https://github.com/geoffgeorgein/Netflix-clone/blob/master/Screenshot%202023-04-18%20125556.png?raw=true' alt='netflix clone'>
 
                         </img>
                     </div>
 
-                    <h2>Title</h2>
-                    <p> lorem ipsum</p>
+                    
 
                     </div>
                 </div>
-
-                <div className='right'></div>
             </div>
         </div>
 
