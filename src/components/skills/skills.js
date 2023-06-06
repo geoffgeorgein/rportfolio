02aaecabf4d891f42356ac0b8e4c66ba08874e4d
Geoff George <geoffgeorgethommana@gmail.com>
2023-06-06 13:56:49 +0530

    icons

--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -10,16 +10,16 @@ const Skills = () => {
 
     <div className='skillContainer'>
 
-        <SkillItem src='https://github.com/geoffgeorgein/rportfolio/blob/main/public/assets/html5.png?raw=true' title='HTML'/>
-        <SkillItem src='https://github.com/geoffgeorgein/rportfolio/blob/main/public/assets/css.png?raw=true' title='CSS'/>
+        <SkillItem src='https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg' title='HTML'/>
+        <SkillItem src='https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg' title='CSS'/>
         <SkillItem src='https://github.com/geoffgeorgein/rportfolio/blob/main/public/assets/JavaScript-logo.png?raw=true' title='JavaScript'/>
         <SkillItem src='https://github.com/geoffgeorgein/rportfolio/blob/main/public/assets/React-icon.svg.png?raw=true' title='ReactJs'/>
         <SkillItem src='https://github.com/geoffgeorgein/rportfolio/blob/main/public/assets/redux.png?raw=true' title='Redux'/>
         <SkillItem src='https://github.com/geoffgeorgein/rportfolio/blob/main/public/assets/next-js-logo-7929BCD36F-seeklogo.com.png?raw=true' title='NextJs'/>
-        <SkillItem src='https://github.com/geoffgeorgein/rportfolio/blob/main/public/assets/Git-logo.svg.png?raw=true' title='Git'/>
+        <SkillItem src='https://github.com/geoffgeorgein/rportfolio/blob/main/public/assets/Git-Icon.png?raw=true' title='Git'/>
 
-        <SkillItem src='https://github.com/geoffgeorgein/rportfolio/blob/main/public/assets/firebase.png?raw=true' title='Firebase'/>
-        
+        <SkillItem src='https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg' title='Firebase'/>
+        <SkillItem src='https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg' title='Bootstrap'/>
     </div>
     <p></p>
     <p></p>
