5c66349afaf084682a1819becaaa3f268856540f
Geoff George <geoffgeorgethommana@gmail.com>
2023-06-06 17:43:33 +0530

    css

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -11,7 +11,7 @@ const Contact = () => {
     setMessage(true)
   }
   return (
-    <div className='contact' id='contact'>Contact
+    <div className='contact' id='contact'>
 
       <div className='left'>
           <img src='assets/shake.svg' alt='img'></img>
