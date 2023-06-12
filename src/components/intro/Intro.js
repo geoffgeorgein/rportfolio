61bab33a8d2c77c2eabd3327d54bede7ee56eae7
Geoff George <geoffgeorgethommana@gmail.com>
2023-06-12 20:19:46 +0530

    typed animation

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './intro.scss';
+import { init } from 'ityped';
 
 const Intro = () => {
+
+  const textref=useRef();
+
+  useEffect(()=>{
+
+    init(textref.current,{
+      showCursor:true,
+      backSpeed:  100,
+      backDelay:1500,
+      strings:["Developer","Problem Solver","Freelancer"]
+    });
+
+  },[])
+
+
   return (
     <div className='intro' id='intro'>
 
@@ -18,7 +34,7 @@ const Intro = () => {
       <div className='wrapper'>
         <h2>Hi there</h2>
         <h1>Im Geoff</h1>
-        <h3>Im a Developer</h3>
+        <h3>Im a <span ref={textref} > </span> </h3>
 
         <a href='#projects'>
           <img src='/assets/down.png' alt='down'></img>
